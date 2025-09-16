1b7e60d3f0b316739d030704b5ed161aee737528
Hamlet Abbaszade <hamletabbaszade0@gmail.com>
2025-09-16 20:01:39 +0400

    hamlet test edir

--- src/main/java/www/hmn/az/config/Test.java
+++ src/main/java/www/hmn/az/config/Test.java
@@ -2,6 +2,6 @@ package www.hmn.az.config;
 
 @FunctionalInterface
 public interface Test<T, R> {
-     //hello/test//dev/test/branch2
+     //hello/test//dev/test/branch2/men hamlet test edirem brnchmi
      void foo(T t, R r);
 }
